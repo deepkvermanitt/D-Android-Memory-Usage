d5f65c78c10605ffb868ae29620f783ea5b139e7
TFDeep <deep.nitt@gmail.com>
2025-05-25 17:27:49 +0530

    Add AppComponent interface  and module class

---
+++ app/src/main/java/com/deepkverma/d_android_memory_usage/di/AppComponent.kt
@@ -0,0 +1,7 @@
+package com.deepkverma.d_android_memory_usage.di
+
+import dagger.Component
+
+@Component(modules = [AppModule::class])
+interface AppComponent  {
+}
---
+++ app/src/main/java/com/deepkverma/d_android_memory_usage/di/AppModule.kt
@@ -0,0 +1,8 @@
+package com.deepkverma.d_android_memory_usage.di
+
+import dagger.Module
+
+@Module
+class AppModule {
+
+}
